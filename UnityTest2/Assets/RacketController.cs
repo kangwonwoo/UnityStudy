88d7409df292836ba53062209e2d993e5a0ac2d8
kww9496@gmail.com <kww9496@gmail.com>
2022-07-04 16:08:09 +0900

    2022/07/04 수업예제

---
+++ UnityTest2/Assets/RacketController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacketController : MonoBehaviour
+{
+    private Rigidbody racketRd;
+    public float speed = 10.0f;
+    Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        racketRd = GetComponent<Rigidbody>();
+
+        startPos = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+            //  Rigidbody 물리엔진을 이용한 이동
+            //racketRd.AddForce(-speed, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+            //  Rigidbody 물리엔진을 이용한 이동
+            //racketRd.AddForce(+speed, 0, 0);
+        }
+    }
+
+    /*
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("BALL"))
+        {
+            Vector3 currPos = collision.transform.position;
+            Vector3 incomVec = currPos - startPos;
+            Vector3 normalVec = collision.contacts[0].normal;
+            Vector3 reflectVec = Vector3.Reflect(incomVec, normalVec);
+
+            reflectVec = reflectVec.normalized;
+
+            collision.gameObject.GetComponent<Rigid.AddForce(reflectVec * speed);
+        }
+        startPos = transform.position;
+    }
+    */
+}
