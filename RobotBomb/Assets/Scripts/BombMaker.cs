9b892d1532c4082aed6c13df8a835bb363a9f447
kww9496@gmail.com <kww9496@gmail.com>
2022-07-07 17:02:33 +0900

    2022/07/07 수업예제

---
+++ RobotBomb/Assets/Scripts/BombMaker.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombMaker : MonoBehaviour
+{
+    public GameObject bombPrefab;
+    public float interval = 1.0f;
+    float delta = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        delta += Time.deltaTime;  //  1段原陥 持失
+
+        if (delta > interval)
+        {
+            delta = 0;
+            GameObject bomb = Instantiate(bombPrefab);
+            int x = Random.Range(-8, 9);
+            bomb.transform.position = new Vector2(x, 4);
+        }
+    }
+}
